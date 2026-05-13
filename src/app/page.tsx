c86c16f150097d72665797fa9302def07fa05f2d
Vihat Technology <patelishu2707@gmail.com>
2026-05-13 17:16:11 +0530

    Update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import API from "@/services/api";
-
 export default function Home() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    const fetchBackend = async () => {
-      try {
-        const res = await API.get("/");
-        setMessage(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchBackend();
-  }, []);
-
   return (
     <div className="bg-black text-white min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">{message}</h1>
+      <h1 className="text-5xl font-bold">
+        Welcome to ExamRanker 🚀
+      </h1>
     </div>
   );
 }
